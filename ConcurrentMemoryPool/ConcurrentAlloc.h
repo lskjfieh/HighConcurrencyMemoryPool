79fe64a55515a141d4a5b00094ea3903006adbfd
L <lxtwyyx456123@163.com>
2023-08-30 16:54:20 +0800

    并发分配内存

--- ConcurrentMemoryPool/ConcurrentAlloc.h
+++ ConcurrentMemoryPool/ConcurrentAlloc.h
@@ -1,22 +1,56 @@
 #pragma once
-
 #include "Common.h"
 #include "ThreadCache.h"
-
+#include "PageCache.h"
+#include "ObjectPool.h"
 static void* ConcurrentAlloc(size_t size)
 {
-	// 通过TLS 每个线程无锁的获取自己专属的ThreadCache对象
-	if (pTLSThreadCache == nullptr)
+
+	if (size > MAX_BYTES) 
 	{
-		pTLSThreadCache = new ThreadCache;
+		size_t alignSize = SizeClass::RoundUp(size);
+		size_t kpage = alignSize >> PAGE_SHIFT;
+
+		PageCache::GetInstance()->_pageMtx.lock();
+		Span* span = PageCache::GetInstance()->NewSpan(kpage);
+		span->_objSize = size;
+		PageCache::GetInstance()->_pageMtx.unlock();
+
+		void* ptr = (void*)(span->_pageId << PAGE_SHIFT);
+		return ptr;
+	}
+	else
+	{
+		// 通过TLS 每个线程无锁的获取自己专属的ThreadCache对象
+		if (pTLSThreadCache == nullptr)
+		{
+			static ObjectPool<ThreadCache> tcPool;
+			//pTLSThreadCache = new ThreadCache;
+			pTLSThreadCache = tcPool.New();
+		}
+
+		//cout << std::this_thread::get_id() << ":" << pTLSThreadCache << endl;
+
+		return pTLSThreadCache->Allocate(size);
 	}
 
-	cout << std::this_thread::get_id() << ":" << pTLSThreadCache << endl;
-
-	return pTLSThreadCache->Allocate(size);
 }
-static void ConcurrentFree(void* ptr, size_t size)
+static void ConcurrentFree(void* ptr)
 {
-	assert(pTLSThreadCache);
-	pTLSThreadCache->Deallocate(ptr, size);
+	Span* span = PageCache::GetInstance()->MapObjectToSpan(ptr);
+	size_t size = span->_objSize;
+
+	if (size > MAX_BYTES)
+	{
+		
+		PageCache::GetInstance()->_pageMtx.lock();
+		PageCache::GetInstance()->ReleaseSpanToPageCache(span);
+		PageCache::GetInstance()->_pageMtx.unlock();
+	}
+	else
+	{
+		assert(pTLSThreadCache);
+		pTLSThreadCache->Deallocate(ptr, size);
+	}
+
 }
